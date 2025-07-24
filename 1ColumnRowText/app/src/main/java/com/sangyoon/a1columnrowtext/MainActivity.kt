66ae718a7bc7234e283dcaf5c150861e6e25d1aa
Sangyoon98 <qlrqod123123@naver.com>
2025-07-24 12:19:08 +0900

    2025-07-24

--- 1ColumnRowText/app/src/main/java/com/sangyoon/a1columnrowtext/MainActivity.kt
+++ 1ColumnRowText/app/src/main/java/com/sangyoon/a1columnrowtext/MainActivity.kt
@@ -4,21 +4,113 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.sangyoon.a1columnrowtext.ui.theme._1ColumnRowTextTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Hello!");
+//            MainScreen()
+
+            Scaffold { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Green)
+                        .padding(innerPadding)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        "Hello",
+                        style = TextStyle(
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Red,
+                            fontStyle = FontStyle.Italic
+                        )
+                    )
+                    Text("World")
+                    Text("Name")
+                }
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                }
+            )
+        },
+    ) { innerPadding ->
+        ScrollContent(innerPadding)
+    }
+}
+
+@Composable
+private fun ScrollContent(innerPadding: PaddingValues) {
+    LazyColumn(
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        items(
+            count = 100,
+            itemContent = {
+                Text(
+                    text = "Hello",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Green)
+                        .padding(1.dp)
+                )
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    text = "World",
+                    modifier = Modifier.padding(1.dp)
+                )
+            }
+        )
+    }
+}
+
+@Composable
+@Preview
+fun GreetingPreview() {
+    MainScreen()
 }
