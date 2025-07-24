ea47be7dad2ab94ccf68df7371b952f09278214b
Sangyoon98 <qlrqod123123@naver.com>
2025-07-24 10:30:31 +0900

    initial commit

---
+++ 1ColumnRowText/app/src/main/java/com/sangyoon/a1columnrowtext/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sangyoon.a1columnrowtext
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sangyoon.a1columnrowtext.ui.theme._1ColumnRowTextTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _1ColumnRowTextTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _1ColumnRowTextTheme {
+        Greeting("Android")
+    }
+}
