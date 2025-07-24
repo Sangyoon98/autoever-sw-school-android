bdd787605ddfbfcaca1830fe5a3af28221a9e72d
Sangyoon98 <qlrqod123123@naver.com>
2025-07-24 12:36:28 +0900

    2025-07-24

---
+++ 3Box/app/src/main/java/com/sangyoon/a3box/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.sangyoon.a3box
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sangyoon.a3box.ui.theme._3BoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold { innerPadding ->
+                Box(
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+        }
+    }
+}
--- a2composablepreview/app/src/main/java/com/sangyoon/a2composablepreview/MainActivity.kt
+++ a2composablepreview/app/src/main/java/com/sangyoon/a2composablepreview/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.sangyoon.a2composablepreview
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sangyoon.a2composablepreview.ui.theme._2ComposablePreviewTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _2ComposablePreviewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyTextView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MyTextView() {
+    Text(text = "Hello 여러분")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    _2ComposablePreviewTheme {
-        Greeting("Android")
-    }
+private fun MyTextViewPreview() {
+    MyTextView()
+}
+
+@Preview(showBackground = true, name = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(showBackground = true, name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview
+@Composable
+private fun MyTextViewPreview2() {
+    MyTextView()
 }
