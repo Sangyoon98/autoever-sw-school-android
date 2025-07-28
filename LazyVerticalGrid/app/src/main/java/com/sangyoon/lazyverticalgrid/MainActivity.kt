7f0edd9e4f30c9ec095deccaf9dbea3a6e235e5e
Sangyoon98 <qlrqod123123@naver.com>
2025-07-28 16:46:43 +0900

    2025-07-28

--- LazyVerticalGrid/app/src/main/java/com/sangyoon/lazyverticalgrid/MainActivity.kt
+++ LazyVerticalGrid/app/src/main/java/com/sangyoon/lazyverticalgrid/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sangyoon.lazyverticalgrid.ui.theme.LazyVerticalGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +23,22 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LazyVerticalGridTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val numbers = listOf(
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
+            )
+
+            Scaffold { paddingValues ->
+                LazyVerticalGrid(
+                    modifier = Modifier.padding(paddingValues),
+                    columns = GridCells.Fixed(4),   // 한 줄의 이이템 수
+                    horizontalArrangement = Arrangement.spacedBy(8.dp), // 가로 간격
+                    verticalArrangement = Arrangement.spacedBy(16.dp)   // 세로 간격
+                ) {
+                    items(numbers) { number ->
+                        Text(text = number)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyVerticalGridTheme {
-        Greeting("Android")
     }
 }
