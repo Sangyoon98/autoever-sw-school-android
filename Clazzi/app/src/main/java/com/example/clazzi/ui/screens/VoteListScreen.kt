7df77a0ca22778184792111070c1243278d23fca
Sangyoon98 <qlrqod123123@naver.com>
2025-08-06 17:32:54 +0900

    2025-08-06

---
+++ Clazzi/app/src/main/java/com/example/clazzi/ui/screens/VoteListScreen.kt
@@ -0,0 +1,82 @@
+package com.example.clazzi.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.clazzi.model.Vote
+import com.example.clazzi.ui.theme.ClazziTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VoteListScreen(
+    navController: NavController,
+    voteList: List<Vote>,
+    onVoteClicked: (String) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("투표 목록") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("createVote") }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "투표 만들기")
+            }
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            items(voteList) { vote ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onVoteClicked(vote.id) }
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(text = vote.title)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VoteListScreenPreview() {
+    ClazziTheme {
+        VoteListScreen(
+            navController = NavController(context = LocalContext.current),
+            voteList = arrayListOf(),
+            onVoteClicked = {}
+        )
+    }
+}
