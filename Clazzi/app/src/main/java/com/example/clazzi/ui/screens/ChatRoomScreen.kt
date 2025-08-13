ecac202f9fcc0dacfa2632a2106635254d659016
Sangyoon98 <qlrqod123123@naver.com>
2025-08-13 12:23:59 +0900

    2025-08-13

--- Clazzi/app/src/main/java/com/example/clazzi/ui/screens/ChatRoomScreen.kt
+++ Clazzi/app/src/main/java/com/example/clazzi/ui/screens/ChatRoomScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -90,7 +91,9 @@ fun ChatRoomScreen(
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .imePadding()
         ) {
             // 메시지 목록
             LazyColumn(
